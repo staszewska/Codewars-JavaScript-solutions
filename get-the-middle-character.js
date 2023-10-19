33160b704b1adde1e0cdfcc234de0cdfcda21c1c
Malgorzata Staszewska-Bui <mstaszewska93@gmail.com>
2023-10-19 12:48:20 +0200

    Get the middle character

--- get-the-middle-character.js
+++ get-the-middle-character.js
@@ -11,9 +11,17 @@ function getMiddle(s) {
         return s.charAt(middleIndex)
         // If the length of the string is even, we return the two characters at the middle by using the substr method, starting from middleIndex - 1 and extracting 2 characters.
     } else {
-        return s.substr(middleIndex - 1, 2);
+        // return s.substr(middleIndex - 1, 2);
+
+
+        //         middleIndex - 1: This expression subtracts 1 from middleIndex, meaning it's trying to find the character in the string that comes just before the character at the middleIndex.
+
+        // middleIndex + 1: This expression adds 1 to middleIndex, meaning it's trying to find the character in the string that comes just after the character at the middleIndex.
+        return s.substring(middleIndex - 1, middleIndex + 1)
     }
 
 }
+
+console.log(getMiddle("middle"))
 
 
